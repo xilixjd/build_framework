cf24d2792ea177a4b4d10900f5fcd86057970a6e
xiejunda <xiejunda@didichuxing.com>
2019-04-22 21:07:02 +0800

    写了一些diffChildren

--- react15/react15.ts
+++ react15/react15.ts
@@ -1,8 +1,12 @@
+interface Props {
+  children: Array<Vnode>,
+  // [propName: string]: any
+}
 interface Vnode {
-  type: Function|string
-  props: object|null
+  type: Function|string|null
+  props: Props|null
   text: string|null
-  key: string|number|null
+  key: string|null
   ref: object|null
   _children: Array<Vnode>|null
   _dom: HTMLElement|null
@@ -27,8 +31,81 @@ class Component {
     this._componentDom = null
   }
 
-  setState(state: object|Function, callback: Function) {
+  setState(state: object|Function, callback: Function):void {
   }
 }
 
-let c = new Component({}, {})
+const EMPTY_OBJ = {}
+const EMPTY_ARRAY = []
+
+function diffChildren(
+  parentDom: HTMLElement, newParentVnode: Vnode, oldParentVnode: Vnode|null
+):void {
+  let newChildren: Array<Vnode>|null = newParentVnode._children
+  if (!newChildren) {
+    const newPropsChildren = newParentVnode.props.children
+    if (!newPropsChildren) {
+      newParentVnode._children = EMPTY_ARRAY
+    } else {
+      newParentVnode._children = toChildArray(newPropsChildren)
+    }
+    newChildren = newParentVnode._children
+  }
+  const oldChildren: Array<Vnode> = oldParentVnode ? oldParentVnode._children : EMPTY_ARRAY
+  let oldKeyObject: {string?: Vnode} = {}
+  for (let i = 0; i < oldChildren.length; i++) {
+    const oldChild: Vnode = oldChildren[i]
+    // obj 的 key 为 key + type
+    const oldKey: string = (oldChild.key || '') + (oldChild.type ? oldChild.type.toString() : '')
+    oldKeyObject[oldKey] = oldChild
+  }
+  for (let i = 0; i < newChildren.length; i++) {
+    const newChild: Vnode = newChildren[i]
+    const newKey: string = (newChild.key || '') + (newChild.type ? newChild.type.toString() : '')
+    const oldChild: Vnode = oldChildren[i]
+    const oldChildDom: HTMLElement|undefined = oldChild[i]._dom
+    let newChildDom: HTMLElement
+    if (newKey in oldKeyObject) {
+      newChildDom = diff()
+    } else {
+      newChildDom = mount()
+    }
+  }
+}
+
+function mount():HTMLElement {
+  return document.createElement('div')
+}
+
+function diff():HTMLElement {
+  return document.createElement('div')
+}
+
+function toChildArray(children: Array<Vnode>|string):Array<Vnode> {
+  if (Array.isArray(children)) {
+    return [...children]
+  } else {
+    return [createElement(null, null, children)]
+  }
+}
+
+function createElement(type, props, children):Vnode {
+  return createVnode(type, props, '', '', {})
+}
+
+function createVnode(
+  type: Function|string, props: Props|null, 
+  text: string|null, key: string|null, ref: object|null
+):Vnode {
+  const vnode: Vnode = {
+    type,
+    props,
+    text,
+    key,
+    ref,
+    _children: null,
+    _dom: null,
+    _component: null,
+  }
+  return vnode
+}
