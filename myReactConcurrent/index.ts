a23e688b2ccdc7aafd21c412848b9dc7c3e88079
xilixjd <349729220@qq.com>
2021-03-03 23:22:22 +0800

    feat: render + createElement; next: createDom

--- myReactConcurrent/index.ts
+++ myReactConcurrent/index.ts
@@ -1,3 +1,89 @@
-function createElement() {}
+enum EffectTag {
+  INSERT = 0b00000001,
+  UPDATE = 0b00000010,
+  DELETE = 0b00000100,
+}
 
-function render(reactElement, dom) {}
+const TEXT_ELEMENT = 'TEXT_ELEMENT'
+
+interface Ref<T = unknown> {
+  current: T
+}
+
+interface IElement extends Record<string, unknown> {
+  type: string | Function
+  props: IElementProps
+  key?: string | number
+  ref?: Ref
+}
+
+interface IElementProps extends Record<string, unknown> {
+  key?: string | number
+  children?: IElement[]
+  ref? : Ref
+  nodeValue?: number | string
+}
+
+interface IFiber extends IElement {
+  child?: IFiber
+  parent?: IFiber
+  sibling?: IFiber
+
+  dom?: Element
+
+  time: number
+}
+
+function Fragment(props: IElementProps) {
+  return props.children
+}
+
+function createElement(type: string | Function, attrs: IElementProps, ...childrenElements: (IFiber | string)[]): IElement {
+  const props = attrs || {}
+  const children: IElement[] = []
+  const key = attrs.key;
+  const ref = attrs.ref || null;
+  for (let i = 0; i < childrenElements.length; i++) {
+    const child = childrenElements[i]
+    if (child) {
+      if (typeof child === 'string' || typeof child === 'number') {
+        const element = createTextElement(child)
+        children.push(element)
+      } else {
+        children.push(child)
+      }
+    }
+  }
+  props.children = children
+  return {
+    type,
+    props,
+    key,
+    ref,
+  }
+}
+
+function createTextElement(text: string | number): IElement {
+  return {
+    type: TEXT_ELEMENT,
+    props: {
+      nodeValue: text,
+    }
+  }
+}
+
+function render(reactElement: IElement, dom: Element) {
+  const rootFiber = {
+    dom,
+    props: {
+      children: [reactElement]
+    },
+    time: 0,
+    type: null,
+  }
+  dispatchUpdate(rootFiber)
+}
+
+function dispatchUpdate(fiber: IFiber) {
+  
+}
